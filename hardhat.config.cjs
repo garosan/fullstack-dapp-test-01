2ba3c0fd72c1a33398a266f4d91ce8af9feb0fc5
Garosan <garosanchez27@gmail.com>
2024-11-26 23:25:54 -0600

    Add envs to deploy to different networks

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -1,4 +1,12 @@
 require("@nomicfoundation/hardhat-toolbox");
+// require("dotenv").config();
+require("dotenv").config({ path: ".env.mainnet.prod" });
+const SEPOLIA_API_URL = process.env.API_URL;
+const SEPOLIA_PRIVATE_KEY = process.env.PRIVATE_KEY;
+const BASE_API_URL = process.env.BASE_API_URL;
+const BASE_MAIN_PRIVATE_KEY = process.env.BASE_MAIN_PRIVATE_KEY;
+const ARBITRUM_API_URL = process.env.ARBITRUM_API_URL;
+const ARBITRUM_MAIN_PRIVATE_KEY = process.env.ARBITRUM_MAIN_PRIVATE_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -8,5 +16,19 @@ module.exports = {
     artifacts: "./src/backend/artifacts",
     cache: "./src/backend/cache",
     tests: "./src/backend/test",
+  },
+  networks: {
+    // sepolia: {
+    //   url: SEPOLIA_API_URL,
+    //   accounts: [SEPOLIA_PRIVATE_KEY],
+    // },
+    baseMain: {
+      url: BASE_API_URL,
+      accounts: [BASE_MAIN_PRIVATE_KEY],
+    },
+    arbitrumMain: {
+      url: ARBITRUM_API_URL,
+      accounts: [ARBITRUM_MAIN_PRIVATE_KEY],
+    },
   },
 };
